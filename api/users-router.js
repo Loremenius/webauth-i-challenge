2891266e1598103f00bce63a7d15ef86837897fc
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-12-16 15:16:09 -0700

    added migration to add users table. added api functionality to register, login, and get users

--- api/users-router.js
+++ api/users-router.js
@@ -6,4 +6,53 @@ const router = express.Router();
 
 const bcrypt = require('bcryptjs');
 
+router.get('/users', (req,res)=>{
+    db.find()
+        .then(data=>{
+            res.status(200).json(data);
+        })
+        .catch(error=>{
+            console.log(error);
+            res.status(500).json({
+                error:error,
+                errorMessage: 'error getting users'
+            });
+        })
+});
+
+router.post('/register', (req,res)=>{
+    req.body.password = bcrypt.hashSync(req.body.password,8);
+    db.createUser(req.body)
+        .then(data=>{
+            res.status(201).json(data);
+        })
+        .catch(error=>{
+            console.log(error);
+            res.status(500).json({
+                error:error,
+                errorMessage: 'error creating user'
+            });
+        })
+});
+
+router.post('/login', (req,res)=>{
+    db.findByName(req.body.username)
+        .then(data=>{
+            console.log(data);
+            if(data && bcrypt.compareSync(req.body.password, data.password)){
+                res.status(200).json({message:`Welcome ${data.username}`});
+            }else{
+                res.status(401).json({message:'Invalid Credentials'});
+            }
+        })
+        .catch(error=>{
+            console.log(error);
+            res.status(500).json({
+                error:error,
+                errorMessage: 'error logging in  user'
+            });
+        })
+});
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server = express();
 
 server.use(express.json());
 
-server.use("/api/users",usersRouter);
+server.use("/api/",usersRouter);
 
 module.exports = server;
